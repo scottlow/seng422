6cc2f044fbfd9e9fa9a9f415e7512e9e24f62259
mikkosan <mikkosanchez@gmail.com>
2014-07-18 16:56:49 -0700

    minimalized weather section

--- client/app/scripts/directives/lscsweather.js
+++ client/app/scripts/directives/lscsweather.js
@@ -17,9 +17,9 @@ angular.module('clientApp')
           success: function(weather) {
             var html;
             html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-            html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-            html += '<li class="currently">'+weather.currently+'</li>';
-            html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+            //html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+            //html += '<li class="currently">'+weather.currently+'</li>';
+            //html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
   
             angular.element("#weather").html(html);
           },

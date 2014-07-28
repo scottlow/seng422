93cd6ee50f1347ff7d2a2f534c2258818d792bab
Bill Xiong <billxiong99@gmail.com>
2014-07-28 16:50:28 -0700

    Added the Konami code.

---
+++ client/app/scripts/konami.js
@@ -0,0 +1,21 @@
+//check to make sure that the browser can handle window.addEventListener
+if (window.addEventListener) {
+  //create the keys and konami variables
+  var keys = [],
+  konami = "38,38,40,40,37,39,37,39,66,65";
+
+  //bind the keydown event to the Konami function
+  window.addEventListener("keydown", function(e){
+    //push the keycode to the 'keys' array
+    keys.push(e.keyCode);
+
+    //and check to see if the user has entered the Konami code
+    if (keys.toString().indexOf(konami) >= 0) {
+      //do something such as:
+      console.log('Konami');
+      window.location.replace("http://www.danbarham.com/dinklage/?");
+      //and finally clean up the keys array
+      keys = [];
+    };
+  }, true);
+};

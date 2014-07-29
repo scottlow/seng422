a3976ee830fee4edc7e0581e9514a5cb03e34943
scottlow <slow@uvic.ca>
2014-07-28 21:25:24 -0700

    Recommitting old settings because I think last commit broke things.

--- server/lscs_server/lscs_server/settings.py
+++ server/lscs_server/lscs_server/settings.py
@@ -70,17 +70,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.sqlite3',
-        'NAME' : os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lscs',
+        'USER': 'lscs',
+        'PASSWORD': 'LSCS4l!fe',
+        'HOST': 'lscs.db.10377017.hostedresource.com',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'lscs',
-    #     'USER': 'lscs',
-    #     'PASSWORD': 'LSCS4l!fe',
-    #     'HOST': 'lscs.db.10377017.hostedresource.com',
-    # }
 }   
 
 # Internationalization
